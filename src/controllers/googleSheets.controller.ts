0f3e3db555e270cd8749d1f3a07efb3438581a26
Nikita Kutsokon <nikitakutsokon@gmail.com>
2023-09-12 22:10:25 +0300

    feat: create google sheets: service, controller, router

---
+++ src/controllers/googleSheets.controller.ts
@@ -0,0 +1,7 @@
+import { Request, Response } from 'express';
+import * as GoogleSheetsService from '../services/googleSheets.service';
+
+export const updateData = async (req: Request, res: Response) => {
+    //TODO: implement this function: 
+    // depends on values in request data invoke appropriate function from service
+};
