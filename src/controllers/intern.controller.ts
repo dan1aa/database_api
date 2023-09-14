bd5b519d1c67a8bfadba8d25cbb410c5f1d3e694
Nikita Kutsokon <nikitakutsokon@gmail.com>
2023-09-14 22:07:07 +0300

    refactor: remove try catch from intern controller

--- src/controllers/intern.controller.ts
+++ src/controllers/intern.controller.ts
@@ -1,23 +1,16 @@
-import { Request, Response } from 'express';
+import { NextFunction, Request, Response } from 'express';
 import * as InternService from '../services/intern.service';
 
-export const getInternById = async (req: Request, res: Response) => {
-    try {
-        const internId = Number(req.params.id);
-        const result = await InternService.getInternById(internId);
-
-        res.status(200).json(result);
-    } catch(e) {
-        //TODO: Implement Error middleware
-    }
+export const getInternById = async (req: Request, res: Response, next: NextFunction) => {
+    const internId = Number(req.params.id);
+    const result = await InternService.getInternById(internId);
+        
+    res.status(200).json(result);
 };
 
 export const filterInterns = async (req: Request, res: Response) => {
-    try {
-        const filteringParams = req.query;
-        const result = await InternService.filterInterns(filteringParams);
-        res.status(200).json(result);
-    } catch(e) {
-        //TODO: Implement Error middleware
-    } 
+    const filteringParams = req.query;
+    const result = await InternService.filterInterns(filteringParams);
+
+    res.status(200).json(result);
 };
