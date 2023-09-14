d20d015273c6fe5ad1b65cdb5f4e0e73f93d8489
Nikita Kutsokon <nikitakutsokon@gmail.com>
2023-09-14 22:07:53 +0300

    feat: use error handler in app

--- src/index.ts
+++ src/index.ts
@@ -1,21 +1,26 @@
-import express from 'express'; 
 import cors from 'cors'; 
+import express from 'express'; 
 import bodyParser from 'body-parser'
 
 import InternRouter from './routes/intern.route';
 import GoogleSheetsRouter from './routes/googleSheets.route';
+import errorHandler from './middlewares/errorHandler.middleware';
+import asyncErrorHandler from './middlewares/tryCatchMiddleware.middleware';
+import tryCatchMiddleware from './middlewares/tryCatchMiddleware.middleware';
 
 const app = express();
 
 app.use(express.json()); 
+app.use(bodyParser.json());
 app.use(cors({ origin: '*' })); 
 
 app.use('/api', InternRouter); 
 app.use('/api', GoogleSheetsRouter);
 
-app.use(bodyParser())
+app.use(errorHandler);
 
 const PORT = process.env.PORT || 5000;
+
 app.listen(PORT, () => {
     console.log(`Server is online on port ${PORT}`);
 });
