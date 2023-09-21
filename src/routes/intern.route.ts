01296660d45b7783370cacb9696115d2ef10f26b
dan1aa <burmeidan@gmail.com>
2023-09-21 16:18:04 +0300

    updating sheets

--- src/routes/intern.route.ts
+++ src/routes/intern.route.ts
@@ -5,26 +5,6 @@ import tryCatchMiddleware from '@middlewares/tryCatchMiddleware.middleware';
 
 const router = Router();
 
-/**
- * @swagger
- * /interns/{id}:
- *   get:
- *     summary: Get an intern by ID
- *     tags: [Interns]
- *     parameters:
- *       - in: path
- *         name: id
- *         required: true
- *         description: The ID of the intern
- *         schema:
- *           type: integer
- *     responses:
- *       200:
- *         description: Successful response
- *       404:
- *         description: Intern not found
- */
-
 router.get('/interns', tryCatchMiddleware(InternController.filterInterns));
 router.get('/interns/:id', tryCatchMiddleware(InternController.getInternById));
 
