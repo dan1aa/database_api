04011d93bec70464a7fb8ecfbc4dc6ed2ebcc9cb
dan1aa <burmeidan@gmail.com>
2024-02-08 15:42:24 +0200

    fix bug w delete & stop pm2

--- src/services/github-webhooks.service.ts
+++ src/services/github-webhooks.service.ts
@@ -2,36 +2,28 @@ import { exec } from "child_process";
 import { Response } from "express";
 
 export const restartAppOnGithubUpdate = async (res: Response) => {
-    exec('git stash', (err, stdout, stderr) => {
-        if (err) {
-          return JSON.stringify(err)
-        }
+  exec('git stash', (err, stdout, stderr) => {
+    if (err) {
+      return JSON.stringify(err)
+    }
+    exec('cd database_api/ && git pull origin master', (err, stdout, stderr) => {
+      if (err) {
+        return JSON.stringify(err)
+      }
 
-        exec('pm2 stop index && pm2 delete index', (err, stdout, stderr) => {
-            if (err) {
-              return JSON.stringify(err)
-            }
+      exec('tsc', (err, stdout, stderr) => {
 
-              exec('cd database_api/ && git pull origin master', (err, stdout, stderr) => {
-                if (err) {
-                  return JSON.stringify(err)
-                }
+        exec('pm2 restart index', (err, stdout, stderr) => {
+          if (err) {
+            return JSON.stringify(err)
+          }
 
-                exec('tsc', (err, stdout, stderr) => {
+        });
 
-                  exec('pm2 start ./dist/index.js --watch', (err, stdout, stderr) => {
-                    if (err) {
-                      return JSON.stringify(err)
-                    }
-  
-                  });
-  
-                });
-
-              });
-        
-
-          });
-  
       });
+
+    });
+
+
+  });
 }
