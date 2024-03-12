793bf9a927d0269a4aa46dfc2b0cd4c030130d16
dan1aa <burmeidan@gmail.com>
2024-03-12 14:45:48 +0200

    update course result

--- dist/request-schemas/course-result.request-shema.js
+++ dist/request-schemas/course-result.request-shema.js
@@ -9,7 +9,7 @@ exports.createCourseResultScheme = joi_1.default.object({
     internId: joi_1.default.string().required(),
     courseId: joi_1.default.string().required(),
     masteryResult: joi_1.default.string().required(),
-    englishLevel: joi_1.default.string().required()
+    englishLevel: joi_1.default.string().allow(null)
 });
 exports.createCourseResultsScheme = joi_1.default.object({
     data: joi_1.default.array().items(exports.createCourseResultScheme)
--- src/request-schemas/course-result.request-shema.ts
+++ src/request-schemas/course-result.request-shema.ts
@@ -4,7 +4,7 @@ export const createCourseResultScheme = Joi.object({
     internId: Joi.string().required(), // explorer id
     courseId: Joi.string().required(), //course cipher
     masteryResult:  Joi.string().required(),
-    englishLevel: Joi.string().required()
+    englishLevel: Joi.string().allow(null)
 });
 
 export const createCourseResultsScheme = Joi.object({
